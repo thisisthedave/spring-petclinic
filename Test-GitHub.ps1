28985f0e09cd36dc414f674c3196ae8ceb52594b
thisisthedave <thisisthedave@gmail.com>
2022-10-26 16:44:26 -0700

    Remove secret printout

--- Test-GitHub.ps1
+++ Test-GitHub.ps1
@@ -7,5 +7,4 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-Write-Host "TeamCityToken is $TeamCityToken"
 Get-GitHubPullRequest -Owner WSStudios -RepositoryName tdp1 -Number 5736 -Token $(ConvertTo-SecureString -AsPlainText -Force -String "$TeamCityToken")
